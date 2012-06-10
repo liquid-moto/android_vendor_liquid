682236f020ef28c47815f3789bfbf102f4ed90f2
Dustin Einglett <x13thangelx@gmail.com>
2012-06-10 00:05:42 -0500

    add droid2,droid2we, and shadow
    Change-Id: I10a62f10340d3f36533d77aefcdf77e43961ea44

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,6 @@ add_lunch_combo liquid_maguro-userdebug
 add_lunch_combo liquid_mecha-userdebug
 add_lunch_combo liquid_toro-userdebug
 add_lunch_combo liquid_triumph-userdebug
+add_lunch_combo liquid_droid2we-userdebug
+add_lunch_combo liquid_droid2-userdebug
+add_lunch_combo liquid_shadow-userdebug
