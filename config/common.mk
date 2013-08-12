cf6cd6984014b0498bd82bc9e223755aadf97204
roeblatt3 <roeblatt3@yahoo.com>
2013-08-12 10:19:41 -0400

    Switch SuperSU to Superuser and change build number

--- config/common.mk
+++ config/common.mk
@@ -3,7 +3,7 @@ PRODUCT_BRAND ?= liquid
 
 # build
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_DISPLAY_ID="Liquid ICS v1.6"
+    BUILD_DISPLAY_ID="Liquid ICS v2.0"
 
 # overrides
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -94,7 +94,7 @@ PRODUCT_COPY_FILES += \
     vendor/liquid/prebuilt/common/app/OneTimeInitializer.apk:system/app/OneTimeInitializer.apk \
     vendor/liquid/prebuilt/common/app/Phonesky.apk:system/app/Phonesky.apk \
     vendor/liquid/prebuilt/common/app/SetupWizard.apk:system/app/SetupWizard.apk \
-    vendor/liquid/prebuilt/common/app/SuperSU.apk:system/app/SuperSU.apk \
+    vendor/liquid/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk \
     vendor/liquid/prebuilt/common/app/Talk.apk:system/app/Talk.apk \
     vendor/liquid/prebuilt/common/app/Torch.apk:system/app/Torch.apk \
     vendor/liquid/prebuilt/common/app/VoiceSearch.apk:system/app/VoiceSearch.apk
